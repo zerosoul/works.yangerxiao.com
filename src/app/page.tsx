c39c11bc53b08a6c623f2ec1093d53e02406aa3c
Tristan Yang <yanggc888@163.com>
2024-12-12 16:41:12 +0800

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,115 @@
-import Image from "next/image";
-
+// import { Button } from "@nextui-org/react";
+import works from "@/works.json";
+import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
+import { Badge } from "@nextui-org/badge";
+import { Button } from "@nextui-org/button";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import { MdLink } from "react-icons/md";
+const BgPlaceholder = "placeholder.jpg";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="px-5 py-8 xl:p-8">
+      <hgroup className="flex items-center flex-col gap-4 pb-10 text-white">
+        <Badge
+          // placement="bottom-right"
+          classNames={{
+            badge: "bg-transparent right-0",
+          }}
+          showOutline={false}
+          content={works.length}
+          // isOneChar
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          color="warning"
+          variant="flat"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <h1 className="text-3xl md:text-4xl xl:text-6xl font-bold uppercase">
+            Tristan's Works
+          </h1>
+        </Badge>
+        <h2 className="capitalize font-semibold text-lg md:text-xl relative">
+          just for fun
+        </h2>
+      </hgroup>
+
+      <div className="w-fit m-auto grid gap-x-8 gap-y-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
+        {works.map((w) => {
+          const { title, intro, link = "", cover = "", github = "" } = w;
+          const bgValue = cover.startsWith("http")
+            ? `url(${cover})`
+            : `url('https://static.nicegoodthings.com/uPic/co-link/${
+                cover || BgPlaceholder
+              }')`;
+          return (
+            <Card
+              as={Link}
+              href={link || github}
+              target="_blank"
+              radius="sm"
+              style={{
+                backgroundImage: `${bgValue}`,
+              }}
+              key={title}
+              isHoverable={false}
+              isBlurred
+              classNames={{
+                base: "max-w-80 transition-all hover:scale-110 cursor-pointer 2xl:max-w-96 bg-slate-900 bg-center bg-cover bg-no-repeat aspect-square",
+                body: "min-h-40",
+                footer: "gap-3",
+              }}
+            >
+              <CardHeader>
+                <h2
+                  style={{ textShadow: "black 1px 5px 5px" }}
+                  className="font-bold text-xl md:text-2xl xl:text-3xl 2xl:text-4xl text-slate-50"
+                >
+                  {title}
+                </h2>
+              </CardHeader>
+              <CardBody>
+                <p
+                  style={{ textShadow: "black 0px 0px 8px" }}
+                  className="text-gray-200 text-sm md:text-base"
+                >
+                  {intro}
+                </p>
+              </CardBody>
+              <CardFooter>
+                {link && (
+                  <Button
+                    className="opacity-70 hover:opacity-100"
+                    variant="shadow"
+                    color="primary"
+                    as={Link}
+                    href={link}
+                    target="_blank"
+                    radius="full"
+                    size="sm"
+                    startContent={<MdLink className="size-4" />}
+                  >
+                    Visit
+                  </Button>
+                )}
+                {github && (
+                  <Button
+                    as={Link}
+                    href={github}
+                    target="_blank"
+                    className="opacity-70 hover:opacity-100"
+                    variant="shadow"
+                    color="primary"
+                    radius="full"
+                    size="sm"
+                    startContent={<FaGithub className="size-4" />}
+                  >
+                    Source Code
+                  </Button>
+                )}
+              </CardFooter>
+            </Card>
+          );
+        })}
+      </div>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,20 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [
+    nextui({
+      prefix: "ygc",
+    }),
+  ],
 } satisfies Config;
