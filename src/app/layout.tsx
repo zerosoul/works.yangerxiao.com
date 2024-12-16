ba0c699fe9a02ab873eab8b04eff53cecb4abbaf
Tristan Yang <yanggc888@163.com>
2024-12-16 15:59:28 +0800

    feat: plausible code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
 import Footer from "../components/footer";
+import PlausibleCode from "../components/plausible";
 
 export const metadata: Metadata = {
   title: "Tristan's Works",
@@ -27,6 +28,7 @@ export default function RootLayout({
       >
         <Providers>{children}</Providers>
         <Footer />
+        {process.env.NODE_ENV == "production" && <PlausibleCode />}
       </body>
     </html>
   );
